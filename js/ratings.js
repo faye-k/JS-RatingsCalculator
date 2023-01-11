8f736b19fcd11bb3200d44cd06aebd25c1ef7ac4
faye-k <68894840+faye-k@users.noreply.github.com>
2023-01-11 11:21:16 +0100

    Completed module1

--- js/ratings.js
+++ js/ratings.js
@@ -1,4 +1,5 @@
 function collect_ratings(){
+
   let ratings={
     count:0,
     sum:0,
@@ -9,17 +10,27 @@ function collect_ratings(){
 
   const elements=document.querySelectorAll(".rating");
 
-  elements.forEach(function(let element) {
+  elements.forEach(function( element) {
     // code
-    rating=parseInt(element.id.replace('start',''));
-
+    rating=parseInt(element.id.replace('star',''));
 
     ratings.count+=parseInt(element.value);
-    ratings.sum+=parseInt(element.value*rating);
+    ratings.sum+=parseInt(element.value)*rating;
   });
 
   if(ratings.count!=0){
     ratings.average=ratings.sum/ratings.count;
   }
 
+  return ratings;
 }
+
+//Below the collect_ratings() function, add an event listener that listens
+//for the change event on document. When creating the event listener,
+//pass an arrow function as the handler.
+document.addEventListener('change', (event) => {
+  let ratings=collect_ratings();
+
+  let element=document.querySelector("#average");
+  element.value=ratings.average.toFixed(2);
+});
